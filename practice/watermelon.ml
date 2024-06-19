95ad0f194bfa6b795f0a157ba1ea6b4263ff008c
kayumm <92858412+kayumm@users.noreply.github.com>
2024-06-19 19:40:34 +0600

    Update watermelon.ml

--- practice/watermelon.ml
+++ practice/watermelon.ml
@@ -1,6 +1,6 @@
 let () =
   let inp = Scanf.scanf "%d\n" (fun x -> x) in
-  if inp mod 2 = 0 then 
-    Printf.printf "YES"
-  else
+  if inp <= 2 || inp mod 2 <> 0 then 
     Printf.printf "NO"
+  else
+    Printf.printf "YES"
