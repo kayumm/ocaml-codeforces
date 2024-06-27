1ee7f606403d72c2715c05b10cbca71a293ae42e
kayumm <92858412+kayumm@users.noreply.github.com>
2024-06-27 15:43:24 +0600

    Update bit++.ml

--- practice/bit++.ml
+++ practice/bit++.ml
@@ -1,6 +1,6 @@
 let solve () = 
-  let inp = read_int () in
-  let count = ref 0 in
+  let inp = read_int () 
+  and count = ref 0 in
   for _ = 0 to inp - 1 do
     let str = read_line () in
     if str.[0] = '+' || str.[1] = '+' then count := !count + 1
