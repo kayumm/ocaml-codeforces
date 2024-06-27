f4a91c6eb68f728ffbd3dc3dd3a128e93f046578
kayumm <92858412+kayumm@users.noreply.github.com>
2024-06-27 15:41:07 +0600

    Create bit++.ml

---
+++ practice/bit++.ml
@@ -0,0 +1,11 @@
+let solve () = 
+  let inp = read_int () in
+  let count = ref 0 in
+  for _ = 0 to inp - 1 do
+    let str = read_line () in
+    if str.[0] = '+' || str.[1] = '+' then count := !count + 1
+    else if str.[0] = '-' || str.[1] = '-' then count := !count - 1
+  done;
+  !count |> print_int
+
+let _ = solve ()
